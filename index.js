562f1cf535bbd36e192a187a51cbb88fb4b6dccd
Nikita <khakimov.nikita.vch@gmail.com>
2022-08-29 23:08:30 +0300

    createUserMongoDB

--- index.js
+++ index.js
@@ -1,15 +1,15 @@
 import express from 'express';
 import jwt from 'jsonwebtoken';
 import mongoose from 'mongoose';
+import bcrypt from 'bcrypt';
 import 'dotenv/config';
-import { validationResult } from 'express-validator';
 
+import { validationResult } from 'express-validator';
 import { registerValidation } from './validations/auth.js';
+import UserModel from './models/User.js';
 
 mongoose
-  .connect(
-    `mongodb+srv://admin:${process.env.NODE_ENV_PASSWORD_MD}@cluster0.3fs3npg.mongodb.net/?retryWrites=true&w=majority`
-  )
+  .connect(process.env.NODE_ENV_MONGODB)
   .then(() => console.log('DB ok'))
   .catch((err) => console.log('DB error', err));
 
@@ -17,13 +17,24 @@ const app = express();
 
 app.use(express.json());
 
-app.post('/auth/register', registerValidation, (req, res) => {
+app.post('/auth/register', registerValidation, async (req, res) => {
   const errors = validationResult(req);
   if (!errors.isEmpty()) return res.status(400).json(errors.array());
 
-  return res.json({
-    success: true,
+  const password = req.body.password;
+  const salt = await bcrypt.genSalt(10);
+  const passwordHash = await bcrypt.hash(password, salt);
+
+  const doc = new UserModel({
+    email: req.body.email,
+    fullName: req.body.fullName,
+    avatarUrl: req.body.avatarUrl,
+    passwordHash,
   });
+
+  const user = await doc.save();
+
+  return res.json(user);
 });
 
 // app.post('/auth/login', (req, res) => {
