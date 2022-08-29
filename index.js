67ef03655ebad4122b28d2cebdd5747f1ef264f2
Nikita <khakimov.nikita.vch@gmail.com>
2022-08-29 22:48:54 +0300

    validationUser

--- index.js
+++ index.js
@@ -2,6 +2,9 @@ import express from 'express';
 import jwt from 'jsonwebtoken';
 import mongoose from 'mongoose';
 import 'dotenv/config';
+import { validationResult } from 'express-validator';
+
+import { registerValidation } from './validations/auth.js';
 
 mongoose
   .connect(
@@ -14,20 +17,25 @@ const app = express();
 
 app.use(express.json());
 
-app.get('/', (req, res) => {
-  res.send('11 Hello World!');
+app.post('/auth/register', registerValidation, (req, res) => {
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) return res.status(400).json(errors.array());
+
+  return res.json({
+    success: true,
+  });
 });
 
-app.post('/auth/login', (req, res) => {
-  const token = jwt.sign(
-    {
-      email: req.body.email,
-      fullName: 'Jack',
-    },
-    'secret'
-  );
-  res.json({ success: true, token });
-});
+// app.post('/auth/login', (req, res) => {
+//   const token = jwt.sign(
+//     {
+//       email: req.body.email,
+//       fullName: 'Jack',
+//     },
+//     `${process.env.NODE_ENV_SECRET_WORLD}`
+//   );
+//   res.json({ success: true, token });
+// });
 
 app.listen(4444, (err) => {
   if (err) return console.log(err);
