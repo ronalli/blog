920143cfd3f689900bc007b51afd134dc56c38e2
Nikita <khakimov.nikita.vch@gmail.com>
2022-09-05 23:09:36 +0300

    fix

--- controllers/PostController.js
+++ controllers/PostController.js
@@ -54,7 +54,7 @@ export const getOne = async (req, res) => {
           });
         res.json(doc);
       }
-    );
+    ).populate('user');
   } catch (error) {
     console.log(error);
     res.status(500).json({
